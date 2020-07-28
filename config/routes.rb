23d2b829a851c21912671286199aca2380f94e8a
M3L6H <sisoac@hotmail.com>
2020-07-28 13:23:43 -0700

    Update routes to prevent creation of new users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :users, except: [:new, :edit, :index]
+    # For now, we are not intersted in creating new users
+    resources :users, except: [:create, :new, :edit, :index]
     resource :session, only: [:create, :destroy]
   end
 
