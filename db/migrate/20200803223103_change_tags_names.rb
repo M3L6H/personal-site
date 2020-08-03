1bf4f67a6cfd7ba9d3fa948fca24b65994237a18
M3L6H <sisoac@hotmail.com>
2020-08-03 15:32:18 -0700

    Add null false constraint to tag names

---
+++ db/migrate/20200803223103_change_tags_names.rb
@@ -0,0 +1,5 @@
+class ChangeTagsNames < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :tags, :name, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_222510) do
+ActiveRecord::Schema.define(version: 2020_08_03_223103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2020_08_03_222510) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name", limit: 64
+    t.string "name", limit: 64, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_tags_on_name", unique: true
