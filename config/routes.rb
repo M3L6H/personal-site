4e22f9074d1dbb2054021d3b80327e787cd77e96
M3L6H <sisoac@hotmail.com>
2020-08-04 14:05:53 -0700

    Update routes with project routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    # For now, we are not intersted in creating new users
+    # For now, we are not interested in creating new users
     resources :users, except: [:create, :new, :edit, :index]
     resource :session, only: [:create, :destroy]
+
+    resources :projects, except: [:new, :edit]
   end
 
   root to: "static_pages#root"
