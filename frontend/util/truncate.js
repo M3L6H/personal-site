415df7844f74e267dd5d441d6da902affa7fb603
M3L6H <sisoac@hotmail.com>
2020-08-08 01:02:04 -0700

    Make truncate function smarter

--- frontend/util/truncate.js
+++ frontend/util/truncate.js
@@ -1,5 +1,11 @@
 export default (str, len=128) => {
   if (str.length <= len) return str;
 
-  return str.slice(0, len - 3) + "...";
+  let truncated = str.slice(0, len - 3);
+
+  while (truncated.match(/[^\w]$/)) {
+    truncated = truncated.slice(0, truncated.length - 1);
+  }
+
+  return truncated + "...";
 }
