ed700e91889611fb99ac05ebb820c8cc42181f5b
M3L6H <sisoac@hotmail.com>
2020-09-14 15:14:19 -0700

    Add create action

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -12,5 +12,28 @@ class Api::PostsController < ApplicationController
 
   def show
     @post = Post.find_by(id: params[:id])
+    unless @post
+      render json: { id: ["Could not find post with id #{ params[:id] }"] }, status: 404
+    else
+      render :show
+    end
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.create
+      params[:tags].each do |tag|
+        t = Tag.find_by(name: tag) || Tag.create!(name: tag)
+        Tagging.create(taggable_type: "Post", taggable_id: @post.id, tag_id: t.id)
+      end
+      render :create
+    else
+      render json: convert_errors(@post.errors), status: 422
+    end
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
