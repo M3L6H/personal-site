789811e7d02711a8617446eb19d8900124255e14
M3L6H <sisoac@hotmail.com>
2020-09-13 23:43:55 -0700

    Create index and show actions for posts controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,13 @@
 class Api::PostsController < ApplicationController
+  def index
+    query = params[:query] || ""
+    escaped = "%#{ query.gsub("%", "\\\\\%").gsub("_", "\\\\\_") }%"
+    @posts = Post
+      .joins(:tags)
+      .where("title ILIKE ? OR tags.name ILIKE ?", escaped, escaped)
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
 end
