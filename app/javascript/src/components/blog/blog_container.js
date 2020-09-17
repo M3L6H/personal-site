6e9b91b14af3e86af0e2ce331ca6d312e983a7d4
M3L6H <sisoac@hotmail.com>
2020-09-16 17:23:21 -0700

    Add blog container

---
+++ app/javascript/src/components/blog/blog_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+
+import Blog from './blog';
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Blog);
