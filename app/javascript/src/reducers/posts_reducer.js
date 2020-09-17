ac0ea6d5a5082ba53c872f313259a502753ed1b9
M3L6H <sisoac@hotmail.com>
2020-09-16 17:38:24 -0700

    Create posts reducer

---
+++ app/javascript/src/reducers/posts_reducer.js
@@ -0,0 +1,18 @@
+import { RECEIVE_POSTS, RECEIVE_POST, REMOVE_POST } from '../actions/posts_actions';
+
+export default (state={}, action) => {
+  Object.freeze(state);
+
+  switch(action.type) {
+    case RECEIVE_POSTS:
+      return { ...posts };
+    case RECEIVE_POST:
+      return { ...state, [action.post.id]: action.post };
+    case REMOVE_POST:
+      const newState = Object.assign({}, state);
+      delete newState[action.id];
+      return newState;
+    default:
+      return state;
+  }
+};
