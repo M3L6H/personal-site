713bfffd02ddef4b1b1c9321a1eadc8b15c3ba15
M3L6H <sisoac@hotmail.com>
2020-09-18 09:45:31 -0700

    Add debouncer to post editor

--- app/javascript/src/components/post_editor/post_editor.jsx
+++ app/javascript/src/components/post_editor/post_editor.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 
 import { 
   Container, 
@@ -13,13 +13,59 @@ import { Slate, Editable, withReact } from 'slate-react';
 
 const PostEditor = () => {
   const editor = useMemo(() => withReact(createEditor()), []);
+
+  const saveTimeout = useRef(null);
+
   const [title, setTitle] = useState("");
-  const [value, setValue] = useState([
+  const [document, setDocument] = useState([
     {
       type: "paragraph",
       children: [{ text: "A line of text in a paragraph."}]
     }
   ]);
+
+  const handleChange = (e, other) => {
+    let val;
+    
+    switch(e.currentTarget.data["type"]) {
+      case "title":
+        val = e.currentTarget.value;
+        setTitle(e.currentTarget.value);
+        break;
+      case "document":
+        val = other.value || e.currentTarget.value;
+        setDocument(val);
+        break;
+      default:
+        return;
+    }
+
+    setSaving(true);
+    clearTimeout(saveTimeout.current);
+
+    saveTimeout.current = setTimeout(() => {
+      // const formData = new FormData();
+      // formData.append("user[id]", user.id);
+      // formData.append("user[bio]", val);
+      
+      // updateUser(formData)
+      //   .then(({ type }) => {
+      //     if (type === RECEIVE_USERS_ERRORS) {
+      //       setFlash({
+      //         message: "There was an error saving the bio",
+      //         type: ERROR
+      //       });
+      //     } else if (didMountRef.current) {
+      //       setFlash({
+      //         message: "Bio saved successfully",
+      //         type: SUCCESS
+      //       });
+      //     }
+    
+      //     if (didMountRef.current) setSaving(false);
+      //   });
+    }, 1200)
+  };
 
   return (
     <Container text className="post-editor">
@@ -28,13 +74,13 @@ const PostEditor = () => {
         placeholder="Title"
         fluid
         value={ title }
-        onChange={ (_, { value }) => setTitle(value) }
+        onChange={ handleChange }
       />
       <Segment className="document-editor">
         <Slate
           editor={ editor }
-          value={ value }
-          onChange={ setValue }
+          value={ document }
+          onChange={ handleChange }
         >
           <Editable />
         </Slate>
