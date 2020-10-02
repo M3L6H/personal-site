a1e83f8eebee2564e4adaac8dd729f9e085f3f12
M3L6H <sisoac@hotmail.com>
2020-10-01 17:40:25 -0700

    Add fallback for lack of projects

--- app/javascript/src/components/projects/projects_container.js
+++ app/javascript/src/components/projects/projects_container.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import Projects from './projects';
 
 const mapStateToProps = (state) => ({
-  projects: Object.values(state.entities.projects),
+  projects: Object.values(state.entities.projects || {}),
   commits: state.entities.commits
 });
 
