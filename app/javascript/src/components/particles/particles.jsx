09997b58bb672ccc2260fd4599a001117afa8e42
M3L6H <sisoac@hotmail.com>
2021-07-16 17:15:25 -0700

    Pretty particles wrap nicely

--- app/javascript/src/components/particles/particles.jsx
+++ app/javascript/src/components/particles/particles.jsx
@@ -3,10 +3,25 @@ import { withWindowDimensions } from "../hocs";
 
 const randFloat = (range=2) => Math.random() * range - (range / 2.0);
 
-const numPoints = 100;
-const data = []; // x, y, xVel, yVel
+const NUM_POINTS = 1000;
+const RADIUS = 0.005;
+const RESOLUTION = 8;
+const POINTS_PER_TRI = 3;
+const SLOWNESS = 21;
+const DATA_COUNT = 5;
+let data = []; // x, y, xVel, yVel, idx
+let pos;
+let vel;
 
-for (let i = 0; i < numPoints * 4; ++i) data.push(randFloat() / (Math.floor((i % 4) / 2) * 20 + 1));
+
+for (let i = 0; i < NUM_POINTS * RESOLUTION * POINTS_PER_TRI; ++i) {
+  if (i % (RESOLUTION * POINTS_PER_TRI) === 0) {
+    pos = [randFloat(), randFloat()];
+    vel = [randFloat() / SLOWNESS, randFloat() / SLOWNESS];
+  }
+
+  data = data.concat([...pos, ...vel, i % (RESOLUTION * POINTS_PER_TRI)]);
+}
 
 const initializeWebGL = (canvasRef, windowWidth, windowHeight) => {
   if (!canvasRef.current) return null;
@@ -30,94 +45,6 @@ const initializeWebGL = (canvasRef, windowWidth, windowHeight) => {
   return gl;
 };
 
-// const circleShaders = (gl, numVerts) => {
-//   const vertexIds = new Float32Array(numVerts);
-//   vertexIds.forEach((_, i) => { vertexIds[i] = i; });
-
-//   const idBuffer = gl.createBuffer();
-//   gl.bindBuffer(gl.ARRAY_BUFFER, idBuffer);
-//   gl.bufferData(gl.ARRAY_BUFFER, vertexIds, gl.STATIC_DRAW);
-
-//   const CIRCLE_VERTEX_SHADER = `
-//   precision mediump float;
-
-//   attribute float vertexId;
-//   uniform float numVerts;
-//   uniform vec2 resolution;
-
-//   #define PI radians(180.0)
-
-//   vec2 computeCircleTriangleVertex(float vertexId) {
-//     float numSlices = 8.0;
-//     float sliceId = floor(vertexId / 3.0);
-//     float triVertexId = mod(vertexId, 3.0);
-//     float edge = triVertexId + sliceId;
-//     float angleU = edge / numSlices;  // 0.0 to 1.0
-//     float angle = angleU * PI * 2.0;
-//     float radius = step(triVertexId, 1.5);
-//     return vec2(cos(angle), sin(angle)) * radius;
-//   }
-
-//   float hash(float p) {
-//     vec2 p2 = fract(vec2(p * 5.3983, p * 5.4427));
-//     p2 += dot(p2.yx, p2.xy + vec2(21.5351, 14.3137));
-//     return fract(p2.x * p2.y * 95.4337);
-//   }
-
-//   void main() {
-//     float numSlices = 8.0;
-//     float numVertsPerCircle = numSlices * 3.0;
-//     float circleId = floor(vertexId / numVertsPerCircle);
-//     float numCircles = numVerts / numVertsPerCircle;
-
-//     float u = circleId / numCircles;
-//     float angle = u * PI * 2.0;
-//     float radius = 0.8;
-
-//     vec2 pos = vec2(cos(angle), sin(angle)) * radius;
-
-//     vec2 triPos = computeCircleTriangleVertex(vertexId) * 0.1;
-    
-//     float aspect = resolution.y / resolution .x;
-//     vec2 scale = vec2(aspect, 1);
-    
-//     gl_Position = vec4((pos + triPos) * scale, 0, 1);
-//   }
-//   `;
-
-//   const CIRCLE_FRAGMENT_SHADER = `
-//   precision mediump float;
-
-//   void main() {
-//     gl_FragColor = vec4(0, 0, 1, 1);
-//   }
-//   `;
-
-//   const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-//   const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-//   gl.shaderSource(vertexShader, CIRCLE_VERTEX_SHADER);
-//   gl.shaderSource(fragmentShader, CIRCLE_FRAGMENT_SHADER);
-
-//   gl.compileShader(vertexShader);
-//   if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-//     console.error("Error compiling circle vertex shader", gl.getShaderInfoLog(vertexShader));
-//     return null;
-//   }
-  
-//   gl.compileShader(fragmentShader);
-//   if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-//     console.error("Error compiling circle fragment shader", gl.getShaderInfoLog(fragmentShader));
-//     return null;
-//   }
-
-//   return {
-//     idBuffer,
-//     vs: vertexShader,
-//     fs: fragmentShader
-//   };
-// };
-
 const compileShader = (gl, shaderText, shaderType) => {
   const shader = gl.createShader(shaderType);
   gl.shaderSource(shader, shaderText);
@@ -137,15 +64,29 @@ const compileShaders = gl => {
 
   attribute vec2 a_pos;
   attribute vec2 a_vel;
+  attribute float a_idx;
 
   uniform float u_time;
+  uniform float u_radius;
+  uniform float u_resolution;
+  uniform float u_scale;
 
+  #define PI radians(180.0)
+  
   void main() {
     vec2 pos = a_pos + (a_vel * u_time);
-    float x = mod(pos.x, 2.0) - 1.0;
-    float y = mod(pos.y, 2.0) - 1.0;
+    float w = 2.0 + u_radius + u_radius;
+    float hw = w / 2.0;
+    float x = mod(pos.x, w) - hw;
+    float y = mod(pos.y, w) - hw;
+
+    float triIdx = floor(a_idx / 3.0);
+    float triVertexId = mod(a_idx, 3.0);
+    float edgeId = triVertexId + triIdx;
+    float angle = edgeId / u_resolution * PI * 2.0;
+    float radius = step(triVertexId, 1.5) * u_radius;
     
-    gl_Position = vec4(x, y, 0.0, 1.0);
+    gl_Position = vec4((x + (cos(angle) * radius * u_scale)), y + (sin(angle) * radius), 0.0, 1.0);
     gl_PointSize = 2.0;
   }
   `;
@@ -154,7 +95,7 @@ const compileShaders = gl => {
   precision mediump float;
 
   void main() {
-    gl_FragColor = vec4(0.871,0.431,0.294, 1.0);
+    gl_FragColor = vec4(0.871,0.431,0.294, 0.5);
   }
   `;
 
@@ -169,33 +110,14 @@ let rAF;
 const render = (time, webgl) => {
   time *= 0.001;
   time %= 1000;
-  const { gl, program, posLoc, velLoc } = webgl;
-  gl.vertexAttribPointer(
-    posLoc,
-    2,
-    gl.FLOAT,
-    gl.FALSE,
-    4 * Float32Array.BYTES_PER_ELEMENT,
-    0
-  );
-  gl.vertexAttribPointer(
-    velLoc,
-    2,
-    gl.FLOAT,
-    gl.FALSE,
-    4 * Float32Array.BYTES_PER_ELEMENT,
-    2 * Float32Array.BYTES_PER_ELEMENT
-  );
-
-  gl.enableVertexAttribArray(posLoc);
-  gl.enableVertexAttribArray(velLoc);
+  const { gl, program } = webgl;
 
   const timeLoc = gl.getUniformLocation(program, "u_time");
   gl.uniform1f(timeLoc, time);
 
   // Main rendering
   gl.useProgram(program);
-  gl.drawArrays(gl.POINTS, 0, numPoints);
+  gl.drawArrays(gl.TRIANGLES, 0, NUM_POINTS * RESOLUTION * POINTS_PER_TRI);
   
   rAF = requestAnimationFrame(t => render(t, webgl));
 };
@@ -235,14 +157,53 @@ const Particles = ({ windowHeight, windowWidth }) => {
 
     gl.useProgram(program);
 
-    const posLoc = gl.getAttribLocation(program, "a_pos");
-    const velLoc = gl.getAttribLocation(program, "a_vel");
-
     const dataBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, dataBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
 
-    rAF = requestAnimationFrame(time => render(time, { gl, program, posLoc, velLoc }));
+    const posLoc = gl.getAttribLocation(program, "a_pos");
+    const velLoc = gl.getAttribLocation(program, "a_vel");
+    const idxLoc = gl.getAttribLocation(program, "a_idx");
+
+    gl.vertexAttribPointer(
+      posLoc,
+      2,
+      gl.FLOAT,
+      gl.FALSE,
+      DATA_COUNT * Float32Array.BYTES_PER_ELEMENT,
+      0
+    );
+    gl.vertexAttribPointer(
+      velLoc,
+      2,
+      gl.FLOAT,
+      gl.FALSE,
+      DATA_COUNT * Float32Array.BYTES_PER_ELEMENT,
+      2 * Float32Array.BYTES_PER_ELEMENT
+    );
+    gl.vertexAttribPointer(
+      idxLoc,
+      1,
+      gl.FLOAT,
+      gl.FALSE,
+      DATA_COUNT * Float32Array.BYTES_PER_ELEMENT,
+      4 * Float32Array.BYTES_PER_ELEMENT
+    );
+  
+    gl.enableVertexAttribArray(posLoc);
+    gl.enableVertexAttribArray(velLoc);
+    gl.enableVertexAttribArray(idxLoc);
+
+    const diamLoc = gl.getUniformLocation(program, "u_radius");
+    gl.uniform1f(diamLoc, RADIUS);
+
+    const resolutionLoc = gl.getUniformLocation(program, "u_resolution");
+    gl.uniform1f(resolutionLoc, RESOLUTION);
+
+    const scaleLoc = gl.getUniformLocation(program, "u_scale");
+    gl.uniform1f(scaleLoc, windowHeight / windowWidth);
+
+    rAF = requestAnimationFrame(time => render(time, { gl, program }));
 
     return () => cancelAnimationFrame(rAF);
   }, [windowHeight, windowWidth]);
